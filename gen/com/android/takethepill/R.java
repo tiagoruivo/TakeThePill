c14c5ea0b000429f269de524f79f55c78aa657f0
Alvaro Martin <alvaro.martin.rodriguez@alumnos.upm.es>
2012-05-15 20:26:44 +0200

    Creacion de preferencias

--- gen/com/android/takethepill/R.java
+++ gen/com/android/takethepill/R.java
@@ -35,26 +35,28 @@ public final class R {
         public static final int pills_row=0x7f030002;
     }
     public static final class string {
-        public static final int add_days=0x7f05000d;
-        public static final int add_time=0x7f050006;
-        public static final int alert_dialog_cancel=0x7f05000c;
-        public static final int alert_dialog_multi_choice=0x7f05000a;
-        public static final int alert_dialog_ok=0x7f05000b;
+        public static final int add_days=0x7f05000f;
+        public static final int add_time=0x7f050008;
+        public static final int alert_dialog_cancel=0x7f05000e;
+        public static final int alert_dialog_multi_choice=0x7f05000c;
+        public static final int alert_dialog_ok=0x7f05000d;
         public static final int app_name=0x7f050000;
-        public static final int confirm=0x7f050005;
-        public static final int edit_pill=0x7f050004;
-        public static final int error_day=0x7f050012;
-        public static final int error_hour=0x7f050011;
-        public static final int error_pill=0x7f050014;
-        public static final int error_user=0x7f050013;
-        public static final int hour=0x7f050010;
-        public static final int menu_delete=0x7f050003;
+        public static final int changed_prefs=0x7f050004;
+        public static final int confirm=0x7f050007;
+        public static final int edit_pill=0x7f050006;
+        public static final int error_day=0x7f050014;
+        public static final int error_hour=0x7f050013;
+        public static final int error_pill=0x7f050016;
+        public static final int error_user=0x7f050015;
+        public static final int hour=0x7f050012;
+        public static final int menu_delete=0x7f050005;
         public static final int menu_insert=0x7f050002;
-        public static final int no_days=0x7f050009;
+        public static final int menu_prefs=0x7f050003;
+        public static final int no_days=0x7f05000b;
         public static final int no_pills=0x7f050001;
-        public static final int no_time=0x7f050008;
-        public static final int pill=0x7f05000f;
-        public static final int remove_time=0x7f050007;
-        public static final int user=0x7f05000e;
+        public static final int no_time=0x7f05000a;
+        public static final int pill=0x7f050011;
+        public static final int remove_time=0x7f050009;
+        public static final int user=0x7f050010;
     }
 }
--- src/com/android/takethepill/TakeThePill.java
+++ src/com/android/takethepill/TakeThePill.java
@@ -2,6 +2,7 @@ package com.android.takethepill;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -12,23 +13,32 @@ import android.view.View;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class TakeThePill extends ListActivity {
-	
+
 	private static final int ACTIVITY_CREATE=0;
 	private static final int ACTIVITY_EDIT=1;
 
 	private static final int INSERT_ID = Menu.FIRST;
 	private static final int DELETE_ID = Menu.FIRST + 1;
+	private static final int PREFS_ID = Menu.FIRST + 2;
 
 	private PillsDbAdapter mDbHelper;
-	
+
+	//PREFS
+	public static final String PREFS_NAME = "PrefsFile";
+	public static final String EMAIL_KEY = "email";
+	public static final String PHONE_KEY = "phone";
+	private String mEmail, mPhone;
+
 	/**
 	 * Metodo al que se llama al crear la actividad
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		restorePrefs();
 		setContentView(R.layout.pills_list);
 		mDbHelper = new PillsDbAdapter(this);
 		mDbHelper.open();
@@ -65,6 +75,7 @@ public class TakeThePill extends ListActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 		menu.add(0, INSERT_ID, 0, R.string.menu_insert);
+		menu.add(0, PREFS_ID, 1, R.string.menu_prefs);
 		return true;
 	}
 
@@ -77,7 +88,12 @@ public class TakeThePill extends ListActivity {
 		case INSERT_ID:
 			createPill();
 			return true;
+		case PREFS_ID:
+			//TODO Activity para cambiar las preferencias
+			setPrefs("mr.pujo@gmail.com","650928719");
+			return true;
 		}
+
 		return super.onMenuItemSelected(featureId, item);
 	}
 
@@ -89,7 +105,7 @@ public class TakeThePill extends ListActivity {
 		super.onCreateContextMenu(menu, v, menuInfo);
 		menu.add(0, DELETE_ID, 0, R.string.menu_delete);
 	}
-	
+
 	/**
 	 * Metodo que gestiona la seleccion en menu contextual sobre elementos de la lista 
 	 */	
@@ -111,7 +127,7 @@ public class TakeThePill extends ListActivity {
 		Intent i = new Intent(this, PillEdit.class);
 		startActivityForResult(i, ACTIVITY_CREATE);
 	}	
-	
+
 	/**
 	 * Metodo que se llama cuando un item de la lista es seleccionado. 
 	 * Lanza la actividad de  edicion.
@@ -132,5 +148,27 @@ public class TakeThePill extends ListActivity {
 		super.onActivityResult(requestCode, resultCode, intent);
 		fillData();
 	}
-	
+
+
+	private void restorePrefs(){
+		// Restore preferences
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		mEmail= settings.getString(EMAIL_KEY, "empty");
+		mPhone= settings.getString(PHONE_KEY, "0");
+	}
+
+	private void setPrefs(String email, String phone){
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();	
+
+		editor.putString(EMAIL_KEY, email);
+		editor.putString(PHONE_KEY, phone);
+		
+		editor.commit();
+		
+		Toast toast = Toast.makeText(getApplicationContext(),R.string.changed_prefs, Toast.LENGTH_SHORT);
+		toast.show();
+		
+	}
 }
+
